bb0d2d083c560a33f3e83365bb4bd0404af6cad8
Daniel <daniel.mcpherson@live.co.uk>
2020-12-28 13:35:23 +0000

    Pseudocode finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
+import org.openqa.selenium.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // Makes the driver
+        // Login to the website (https://www.shapeways.com/)
+        // Goes to mz4250's minis (https://www.shapeways.com/designer/mz4250/creations?s=0#more-products)
+        // Function to get the location of the forward button
+        // Gets all the products with document.findElementsByClassName("product-url");
+        // Removes each duplicate of the found products
+        // Gets the current url and saves it
+        // Loops through the found products
+            // Goes to the url of the iteration
+            // Find the "Download Product" with the class "btn sw--margin-bottom-2"
+            // Clicks the found download product button
     }
 }
