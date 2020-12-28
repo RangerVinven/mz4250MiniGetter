a02f5c24d4038df85da174d3b4bb2a82f4537523
Daniel <daniel.mcpherson@live.co.uk>
2020-12-28 14:23:46 +0000

    Before First Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,11 @@
 package com.company;
+import jdk.internal.org.objectweb.asm.tree.IntInsnNode;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-
-    // Function to get the location of the forward button
-    static WebElement forwardBtnGetter(WebDriver driver) {
-        WebElement forwardBtn = driver.findElement(By.className("sw-pagination__nav-circle sw-grid-flex sw-grid-flex--align-center sw-grid-flex--justify-center sw--circle sw--border-grey-1 sw--margin-left-2 sw--text-decoration-none"));
-
-        return forwardBtn;
-    }
 
     public static void main(String[] args) throws InterruptedException {
         // Makes the driver
@@ -38,20 +31,48 @@ public class Main {
         // Goes to mz4250's minis (https://www.shapeways.com/designer/mz4250/creations?s=0#more-products)
         driver.get("https://www.shapeways.com/designer/mz4250/creations?s=0#more-products");
 
-        // Calls the function to get the location of the forward button
-        WebElement forwardBtn = forwardBtnGetter(driver);
+        Thread.sleep(3000);
 
-        // Gets all the products with document.findElementsByClassName("product-url");
-        List<WebElement> products = new ArrayList<>();
-        products.addAll(driver.findElements(By.className("product-url")));
+        // Gets the amount of pages with the xpath
+        int numOfPages = Integer.valueOf(driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div[2]/div/a[6]/span")).getText());
 
-        // Removes each duplicate of the found products
+        // Loops through the number of pages
+        for (int i = 0; i < numOfPages; i++) {
 
+            // Gets all the products with document.findElementsByClassName("product-url");
+            List<WebElement> products = new ArrayList<>();
+            products.addAll(driver.findElements(By.className("product-url")));
 
-        // Gets the current url and saves it
-        // Loops through the found products
-            // Goes to the url of the iteration
-            // Find the "Download Product" with the class "btn sw--margin-bottom-2"
-            // Clicks the found download product button
+            // Removes each duplicate of the found products
+            Set<WebElement> productsNoDuplicates = new LinkedHashSet<>(products);
+            products.clear();
+            products.addAll(productsNoDuplicates);
+
+            // Gets the current url and saves it
+            String currentURL = driver.getCurrentUrl();
+
+            // Loops through the found products
+            for (int x = 0; x < products.size(); x++) {
+                // Goes to the url of the iteration
+                driver.get(products.get(x).getAttribute("href"));
+
+                Thread.sleep(5000);
+
+                // Find the "Download Product" with the class "btn sw--margin-bottom-2"
+                WebElement downloadProduct = driver.findElement(By.className("btn sw--margin-bottom-2"));
+
+                // Clicks the found download product button
+                downloadProduct.click();
+            }
+
+            // Goes back to the saved url
+            driver.get(currentURL);
+
+            Thread.sleep(3000);
+
+            // Gets the url of the next page and goes to it
+            String nextPage = driver.findElement(By.className("sw-pagination__nav-circle sw-grid-flex sw-grid-flex--align-center sw-grid-flex--justify-center sw--circle sw--border-grey-1 sw--margin-left-2 sw--text-decoration-none")).getAttribute("href");
+            driver.get("https://www.shapeways.com" + nextPage);
+        }
     }
 }
