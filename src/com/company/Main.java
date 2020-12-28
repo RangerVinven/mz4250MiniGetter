a95ce893df9c8d0793fd44ed0faca09efb3f8060
Daniel <daniel.mcpherson@live.co.uk>
2020-12-28 18:44:38 +0000

    Hopefully final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.*;
@@ -10,8 +11,8 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         // Makes the driver
-        //System.setProperty("webdriver.gecko.driver", "D:\\webdriver\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "D:\\chromeWebdriver2\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
         // Login to the website (https://www.shapeways.com/)
         driver.get("https://www.shapeways.com/login");
@@ -42,12 +43,15 @@ public class Main {
         scanner.nextLine();
 
         // Goes to mz4250's minis (https://www.shapeways.com/designer/mz4250/creations?s=0#more-products)
-        driver.get("https://www.shapeways.com/designer/mz4250/creations?s=0#more-products");
+        driver.get("https://www.shapeways.com/designer/mz4250/creations?s=48#more-products");
 
         Thread.sleep(5000);
 
         // Gets the amount of pages with the xpath
         int numOfPages = Integer.valueOf(driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div[2]/div/a[6]/span")).getText());
+
+        // For the number in the url (...?s=pageNumber#more-products), it goes up in 48, e.g page 1 is 0, page 2 is 48, page 3 is 96, etc
+        int pageNumber = 0;
 
         // Loops through the number of pages
         for (int i = 0; i < numOfPages; i++) {
@@ -65,38 +69,39 @@ public class Main {
             // In the above url, the "8H9RUT284" is the random characters
             List<String> productIDs = new ArrayList<>();
 
-            for (int q = 0; q <= products.size(); q++) {
+            for (int q = 0; q < products.size(); q++) {
 
                 // Removes the start of the string
                 String productID = products.get(q).getAttribute("href").replace("https://www.shapeways.com/product/", "");
-                System.out.println(productID);
 
                 // Takes out the / to the end (leaving only the product's id)
-                productID = productID.replaceAll("\\/.*", productID);
+                productID = productID.replaceAll("\\/.*", "");
 
                 productIDs.add(productID);
 
             }
 
+            // Removes duplicates from the product IDs
+            Set<String> productIDNoDuplicates = new LinkedHashSet<>(productIDs);
+            productIDs.clear();
+            productIDs.addAll(productIDNoDuplicates);
             System.out.println(productIDs);
 
             // Gets the current url and saves it
             String currentURL = driver.getCurrentUrl();
 
             // Loops through the download links
-            for (int p = 0; p <= productIDs.size(); p++) {
+            for (int p = 0; p < productIDs.size(); p++) {
                 Thread.sleep(3000);
                 driver.get("https://www.shapeways.com/product/download/" + productIDs.get(p));
+                Thread.sleep(1000);
+                driver.get(currentURL);
             }
-
-            // Goes back to the saved url
-            driver.get(currentURL);
 
             Thread.sleep(3000);
 
-            // Gets the url of the next page and goes to it
-            String nextPage = driver.findElement(By.className("sw-pagination__nav-circle sw-grid-flex sw-grid-flex--align-center sw-grid-flex--justify-center sw--circle sw--border-grey-1 sw--margin-left-2 sw--text-decoration-none")).getAttribute("href");
-            driver.get("https://www.shapeways.com" + nextPage);
+            // Goes to the next page of minis
+            driver.get("https://www.shapeways.com/designer/mz4250/creations?s=" + pageNumber + "#more-products");
         }
     }
 }
